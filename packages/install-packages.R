69cd8a293c225d3d29a8a0d04e07107d4cf0d876
Caleb Moses <caleb.moses@stats.govt.nz>
2018-03-19 17:20:07 +1300

    added R packages for installation

---
+++ packages/install-packages.R
@@ -0,0 +1,32 @@
+#' The goal of this source file is to install the tidyverse package.
+#' 
+#' To that aim, it installs dependencies first, starting with cellranger,
+#' which is a dependency for readxl (which is itself a dependency for tidyverse).
+#' 
+#' After installing readxl, it installs a number of other dependencies before
+#' installing tidyverse from source.
+#' 
+#' The readxl and tidyverse packages are stored locally in the packages folder
+#' (this is to get around firewall problems). These are installed using:
+#' install.packages("<package_name>", source = TRUE, repos = NULL)
+#' 
+#' The source = TRUE and repos = NULL arguments are necessary when installing
+#' a package from a local source file (rather than remotely via CRAN).
+#'
+
+
+# install dependencies for readxl from CRAN
+install.packages("cellranger")
+
+# install readxl from source <- this is a dependency for tidyverse
+install.packages("packages/readxl_1.0.0.tar.gz", source = TRUE, repos = NULL)
+
+# install other dependencies for tidyverse from CRAN
+install.packages(c('cli', 'dbplyr', 'forcats', 'ggplot2', 'haven', 'modelr', 
+                   'reprex', 'rvest', 'tidyr'))
+
+# install tidyverse from source
+install.packages("packages/tidyverse_1.2.1.tar.gz", source = TRUE, repos = NULL)
+
+# Other packages
+install.packages("prettydoc")
